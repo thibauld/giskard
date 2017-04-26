6620f7bb69dfc0bb12c1a3b06f304e6ed61f238e
Thibauld Favre <tfavre@gmail.com>
2017-04-26 16:49:28 -0400

    Bot update to reflect 2nd experiment with Majority Judgement

--- bot/addons/001_home.rb
+++ bot/addons/001_home.rb
@@ -40,7 +40,7 @@ END
 Sure thing !
 The majority judgement is [blabla...]
 END
-					:understood_answer=>"Continuer",
+					:understood_answer=>"Ok bien compris",
 					:understood=><<-END,
 Top #{Bot.emoticons[:thumbs_up]}
 Passons au vote alors !
@@ -53,18 +53,12 @@ END
 Passons à présent au 2nd vote concernant 4 candidat(e)s finalistes de leur primaire respective mais non-qualifiés :
 attachment:
 END
+					:vote_completed_answer=>"Continuer",
+					:vote_completed=>"Le vote s'est-il correctement déroulé ?",
 					:vote_ok_answer=>"Oui",
 					:vote_ok=>"Content de voir que tout s'est bien passé !\n",
 					:vote_ko_answer=>"Non j'ai eu un souci",
 					:vote_ko=>"Désolé que le vote ne se soit pas bien passé, pouvez-vous nous contacter pour nous dire ce qu'il s'est passé ?\n",
-					:vote_ok_11_answer=>"Oui sans souci",
-					:vote_ok_11=>"Content de voir que tout se soit bien passé !\nPassons à présent au 2nd vote concernant 4 candidat(e)s finalistes de leur primaire respective mais non-qualifiés :",
-					:vote_ko_11_answer=>"Non #{Bot.emoticons['crying_face']}",
-					:vote_ko_11=>"Désolé pour ce souci ! Pouvez-vous nous laisser un message sur https://laprimaire.org/contact/ pour nous dire ce qu'il s'est passé ? Cela nous serait très utile !\n",
-					:vote_ok_4_answer=>"Oui aucun souci",
-					:vote_ok_4=>"Top !\n",
-					:vote_ko_4_answer=>"Non malheureusement",
-					:vote_ko_4=>"Toutes nos excuses pour ce souci, si vous pouviez nous laisser un message sur https://laprimaire.org/contact/ pour nous dire ce qu'il s'est passé, cela nous aiderait beaucoup !\n",
 					:question_jm=>"Est-ce que c'était facile de voter ?",
 					:question_jm_yes_answer=>"Facile",
 					:question_jm_yes=>"Très bonne nouvelle, cette expérimentation est désormais terminée, merci beaucoup pour votre participation.\nN'hésitez pas à partager cette expérimentation à vos amis pour leur faire découvrir le jugement majoritaire.\nEnfin, pour utiliser le jugement majoritaire dans la vraie vie, sachez que LaPrimaire.org l'utilise dans le cadre des primaires ouvertes qu'ils organisent.",
@@ -98,55 +92,45 @@ END
 					:welcome_answer=>"/start",
 					:welcome=><<-END,
 Bonjour #{Bot.emoticons[:smile]}
-Merci de votre participation à cette expérimentation scientifique.
+Merci de votre participation à cette seconde expérimentation scientifique dans le cadre de l'élection présidentielle.
+Les résultats de la première expérimentation seront rendus publics juste après l'élection présidentielle.
 Sachez que votre vote est anonyme et que vos données personnelles ne sont pas enregistrées.
 A tout instant, vous pouvez réinitialiser notre conversation en m'écrivant "/start" (sans les guillemets).
-Pour commencer, connaissez-vous le jugement majoritaire ?
+Question préalable avant de passer au vote : connaissez-vous le jugement majoritaire ?
 END
 					:jm_yes_answer=>"Oui je connais",
 					:jm_yes=><<-END,
-Parfait #{Bot.emoticons[:thumbs_up]}
-A présent, passons au vote !
+Top #{Bot.emoticons[:thumbs_up]}
+Passons directement au vote alors !
 END
 					:jm_no_answer=>"Non, dites m'en plus",
 					:jm_no=><<-END,
-Avec plaisir !
+Avec grand plaisir !
 Le jugement majoritaire est un système de vote inventé par 2 chercheurs français dans le but de permettre aux électeurs de mieux exprimer leurs opinions.
 D'une part les électeurs sont invités à s'exprimer sur *tous* les candidats (au lieu d'en choisir un(e) seul(e)).
 D'autre part, le vote se fait en évaluant chaque candidat avec une mention allant de "Très bien" à "A rejeter".
 Pour plus d'informations sur le jugement majoritaire, n'hésitez pas à consulter la page https://www.jugementmajoritaire2017.com
+Cliquez sur le bouton "Ok bien compris" ci-dessous lorsque vous êtes prêt à continuer.
 END
-					:understood_answer=>"Continuer",
+					:understood_answer=>"Ok bien compris",
 					:understood=><<-END,
-Ok, passons au vote à présent !
+Ok, à présent passons au vote !
 Pour des raisons de confidentialité, le vote ne se fait pas sur Facebook mais sur une page sécurisée.
 Vos votes sont anonymes : Ni Facebook, ni nous n'avons la possibilité de connaître votre vote.
 END
 					:official_candidates=><<-END,
-Voici le 1er vote concernant les 11 candidat(e)s officiel(le)s à l'élection présidentielle :
+Voici les 2 candidats officiels au second tour de l'élection présidentielle :
 attachment:
+Une fois que vous avez voté, cliquez sur le bouton "Continuer"
 END
-					:non_official_candidates=><<-END,
-Passons à présent au 2nd vote concernant 4 candidat(e)s finalistes de leur primaire respective mais non-qualifiés :
-attachment:
-END
-					:vote_ok_answer=>"Oui",
-					:vote_ok=>"Content de voir que tout s'est bien passé !\n",
-					:vote_ko_answer=>"Non j'ai eu un souci",
-					:vote_ko=>"Désolé que le vote ne se soit pas bien passé, pouvez-vous nous laisser un message sur https://laprimaire.org/contact/ pour nous dire ce qu'il s'est passé ? Cela nous serait très utile !\n",
-					:vote_ok_11_answer=>"Oui sans souci",
-					:vote_ok_11=>"Content de voir que tout s'est bien passé !\n",
-					:vote_ko_11_answer=>"Non #{Bot.emoticons['crying_face']}",
-					:vote_ko_11=><<-END,
-Désolé pour ce souci #{Bot.emoticons['disappointed']}
-Pouvez-vous nous laisser un message sur https://laprimaire.org/contact/ pour nous dire ce qu'il s'est passé ? Cela nous serait très utile !
-END
-					:vote_ok_4_answer=>"Oui aucun souci",
-					:vote_ok_4=>"Top !\n",
-					:vote_ko_4_answer=>"Non malheureusement",
-					:vote_ko_4=>"Toutes nos excuses pour ce, si vous pouviez nous laisser un message sur https://laprimaire.org/contact/ pour nous dire ce qu'il s'est passé, cela nous aiderait beaucoup !\n",
+					:vote_completed_answer=>"Continuer",
+					:vote_completed=>"Le vote s'est-il correctement déroulé ?",
+					:vote_ok_answer=>"Oui aucun souci",
+					:vote_ok=>"Top !\n",
+					:vote_ko_answer=>"Non malheureusement",
+					:vote_ko=>"Toutes nos excuses pour ce souci, si vous pouviez nous laisser un message sur https://laprimaire.org/contact/ pour nous dire ce qu'il s'est passé, cela nous aiderait beaucoup !\n",
 					:question_jm=><<-END,
-Trouvez-vous que la manière dont vous venez de voter (en jugeant chacun des candidats) exprime mieux ou moins bien votre opinion, en comparaison avec le « système officiel » (où vous choisissez un seul candidat) ?
+Trouvez-vous que la manière dont vous venez de voter exprime mieux ou moins bien votre opinion, en comparaison avec le « système officiel » ?
 END
 					:question_jm_yes_answer=>"Mieux",
 					:question_jm_yes=><<-END,
@@ -176,7 +160,7 @@ END
 					:share_laprimaire=><<-END,
 Si vous voulez utiliser à nouveau le jugement majoritaire, sachez que LaPrimaire.org l'utilise dans le cadre des primaires ouvertes qu'ils organisent.
 attachment:
-Merci encore et à bientôt pour les résultats !
+Merci encore et à très vite pour les résultats !
 END
 				}
 			}
@@ -210,32 +194,33 @@ END
 				},
 				:official_candidates=>{
 					:callback=>"home/official_candidates_cb",
+					:kbd=>[{"text"=>"home/vote_completed"}],
 					:attachment=>{
 						"type"=>"template",
 						"payload"=>{
 							"template_type"=>"generic",
 							"elements"=>[
 								{
-									"title"=>"Les 11 candidat(e)s officiels",
-									"image_url"=>"https://s3.eu-west-2.amazonaws.com/www.jugementmajoritaire2017.com/images/JM2017-foule900.jpg",
-									"subtitle"=>"Cliquez sur 'Je vote' pour évaluer pour les 11 candidat(e)s officiels.",
+									"title"=>"Les 2 candidats au 2nd tour",
+									"image_url"=>"https://s3.eu-west-2.amazonaws.com/www.jugementmajoritaire2017.com/images/lepenmacron.jpg",
+									"subtitle"=>"Cliquez sur 'Je vote' pour évaluer pour les 2 candidats au Jugement Majoritaire.",
 									"default_action"=> {
 										"type"=>"web_url",
-										"url"=>"https://laprimaire.org/citoyen/vote/facebook_11",
-										#"url"=>"http://localhost:9293/citoyen/vote/facebook_11",
+										"url"=>"https://laprimaire.org/citoyen/vote/facebook_2",
+										#"url"=>"http://localhost:9293/citoyen/vote/facebook_2",
 										#"url"=>"https://laprimaire.org/citoyen/vote/facebook_11?token=a&test=1",
 										"messenger_extensions"=> true,
 										"webview_height_ratio"=> "full",
-										"fallback_url"=>"https://laprimaire.org/citoyen/vote/facebook_11"
-										#"fallback_url"=>"http://localhost:9293/citoyen/vote/facebook_11"
+										"fallback_url"=>"https://laprimaire.org/citoyen/vote/facebook_2"
+										#"fallback_url"=>"http://localhost:9293/citoyen/vote/facebook_2"
 										#"fallback_url"=>"https://laprimaire.org/citoyen/vote/facebook_11?token=a&test=1",
 									},
 									"buttons"=>[
 										{
 											"type"=>"web_url",
 											"title"=>"Je vote !",
-											"url"=>"https://laprimaire.org/citoyen/vote/facebook_11",
-											#"url"=>"http://localhost:9293/citoyen/vote/facebook_11",
+											"url"=>"https://laprimaire.org/citoyen/vote/facebook_2",
+											#"url"=>"http://localhost:9293/citoyen/vote/facebook_2",
 											#"url"=>"https://laprimaire.org/citoyen/vote/facebook_11?token=a&test=1",
 											"webview_height_ratio"=>"full",
 											"webview_share_button"=>"hide"
@@ -246,43 +231,12 @@ END
 						}
 					}
 				},
-				:non_official_candidates=>{
-					:callback=>"home/non_official_candidates_cb",
-					:attachment=>{
-						"type"=>"template",
-						"payload"=>{
-							"template_type"=>"generic",
-							"elements"=>[
-								{
-									"title"=>"Les 4 candidat(e)s finalistes des primaires",
-									"image_url"=>"https://s3.eu-west-2.amazonaws.com/www.jugementmajoritaire2017.com/images/JM2017-foule900.jpg",
-									"subtitle"=>"Cliquez sur 'Je vote' pour évaluer pour 4 candidat(e)s finalistes des primaires.",
-									"default_action"=> {
-										"type"=> "web_url",
-										"url"=>"https://laprimaire.org/citoyen/vote/facebook_4",
-										#"url"=>"http://localhost:9293/citoyen/vote/facebook_4",
-										#"url"=>"https://laprimaire.org/citoyen/vote/facebook_4?token=a&test=2",
-										"messenger_extensions"=> true,
-										"webview_height_ratio"=> "full",
-										"fallback_url"=>"https://laprimaire.org/citoyen/vote/facebook_4"
-										#"fallback_url"=>"http://localhost:9293/citoyen/vote/facebook_4"
-										#"fallback_url"=>"https://laprimaire.org/citoyen/vote/facebook_4?token=a&test=2"
-									},
-									"buttons"=>[
-										{
-											"type"=>"web_url",
-											"title"=>"Je vote !",
-											"url"=>"https://laprimaire.org/citoyen/vote/facebook_4",
-											#"url"=>"http://localhost:9293/citoyen/vote/facebook_4",
-											#"url"=>"https://laprimaire.org/citoyen/vote/facebook_4?token=a&test=2",
-											"webview_height_ratio"=>"full",
-											"webview_share_button"=>"hide"
-										}
-									]      
-								}
-							]
-						}
-					}
+				:vote_completed=>{
+					:answer=>"home/vote_completed_answer",
+					:kbd=>[
+						{"text"=>"home/vote_ok","image_url"=>"https://s3.eu-central-1.amazonaws.com/laprimaire/images/bien.png"},
+						{"text"=>"home/vote_ko","image_url"=>"https://s3.eu-central-1.amazonaws.com/laprimaire/images/insuffisant.png"}
+					]
 				},
 				:vote_ok=>{
 					:answer=>"home/vote_ok_answer",
@@ -292,26 +246,6 @@ END
 				:vote_ko=>{
 					:answer=>"home/vote_ko_answer",
 					:callback=>"home/vote_ko_cb",
-					:jump_to=>"home/question_jm"
-				},
-				:vote_ok_11=>{
-					:answer=>"home/vote_ok_11_answer",
-					:callback=>"home/vote_ok_11_cb",
-					:jump_to=>"home/non_official_candidates"
-				},
-				:vote_ko_11=>{
-					:answer=>"home/vote_ko_11_answer",
-					:callback=>"home/vote_ko_11_cb",
-					:jump_to=>"home/non_official_candidates"
-				},
-				:vote_ok_4=>{
-					:answer=>"home/vote_ok_4_answer",
-					:callback=>"home/vote_ok_4_cb",
-					:jump_to=>"home/question_jm"
-				},
-				:vote_ko_4=>{
-					:answer=>"home/vote_ko_4_answer",
-					:callback=>"home/vote_ko_4_cb",
 					:jump_to=>"home/question_jm"
 				},
 				:question_jm=>{
@@ -351,9 +285,10 @@ END
 							"template_type"=>"generic",
 							"elements"=>[
 								{
-									"title"=>"Découvrez le Jugement Majoritaire",
-									"image_url"=>"https://s3.eu-west-2.amazonaws.com/www.jugementmajoritaire2017.com/images/share-jm-img-logo1.jpg",
-									"subtitle"=>"Une expérience scientifique pour faire progresser la science du vote.",
+									"title"=>"Exprimez-vous avec le Jugement Majoritaire",
+									#"image_url"=>"https://s3.eu-west-2.amazonaws.com/www.jugementmajoritaire2017.com/images/share-jm-img-logo1.jpg",
+									"image_url"=>"https://s3.eu-west-2.amazonaws.com/www.jugementmajoritaire2017.com/images/lepenmacron.jpg",
+									"subtitle"=>"Une expérience scientifique pour tester un système de vote évolué.",
 									"default_action"=> {
 										"type"=> "web_url",
 										"url"=>"https://m.me/JugementMajoritairePresidentielle2017"
@@ -418,7 +353,7 @@ END
 			screen=self.find_by_name("home/vote_paused",self.get_locale(user))
 			return self.get_screen(screen,user,msg)
 		end
-		@users.update_profile(user.sig,'{"connait_JM":1}');
+		@users.update_profile(user.sig,'{"connait_JM_2":1}');
 		return self.get_screen(screen,user,msg)
 	end
 
@@ -428,55 +363,31 @@ END
 			screen=self.find_by_name("home/vote_paused",self.get_locale(user))
 			return self.get_screen(screen,user,msg)
 		end
-		@users.update_profile(user.sig,'{"connait_JM":0}');
+		@users.update_profile(user.sig,'{"connait_JM_2":0}');
 		return self.get_screen(screen,user,msg)
 	end
 
 	def home_question_jm_yes_cb(msg,user,screen)
 		Bot.log.debug "#{__method__}"
-		@users.update_profile(user.sig,'{"mieux_que_SM":1}');
+		@users.update_profile(user.sig,'{"mieux_que_SM_2":1}');
 		return self.get_screen(screen,user,msg)
 	end
 
 	def home_question_jm_no_cb(msg,user,screen)
 		Bot.log.debug "#{__method__}"
-		@users.update_profile(user.sig,'{"mieux_que_SM":0}');
+		@users.update_profile(user.sig,'{"mieux_que_SM_2":0}');
 		return self.get_screen(screen,user,msg)
 	end
 
 	def home_vote_ok_cb(msg,user,screen)
 		Bot.log.debug "#{__method__}"
-		@users.update_profile(user.sig,'{"vote_OK":1}');
+		@users.update_profile(user.sig,'{"vote_OK_2":1}');
 		return self.get_screen(screen,user,msg)
 	end
 
 	def home_vote_ko_cb(msg,user,screen)
 		Bot.log.debug "#{__method__}"
-		@users.update_profile(user.sig,'{"vote_OK":0}');
-		return self.get_screen(screen,user,msg)
-	end
-
-	def home_vote_ok_11_cb(msg,user,screen)
-		Bot.log.debug "#{__method__}"
-		@users.update_profile(user.sig,'{"vote_OK":1,"vote_officiel":1}');
-		return self.get_screen(screen,user,msg)
-	end
-
-	def home_vote_ko_11_cb(msg,user,screen)
-		Bot.log.debug "#{__method__}"
-		@users.update_profile(user.sig,'{"vote_OK":0,"vote_officiel":0}');
-		return self.get_screen(screen,user,msg)
-	end
-
-	def home_vote_ok_4_cb(msg,user,screen)
-		Bot.log.debug "#{__method__}"
-		@users.update_profile(user.sig,'{"vote_non_officiel":1}');
-		return self.get_screen(screen,user,msg)
-	end
-
-	def home_vote_ko_4_cb(msg,user,screen)
-		Bot.log.debug "#{__method__}"
-		@users.update_profile(user.sig,'{"vote_non_officiel":0}');
+		@users.update_profile(user.sig,'{"vote_OK_2":0}');
 		return self.get_screen(screen,user,msg)
 	end
 
@@ -497,13 +408,13 @@ END
 			}
 		}
 		custom_screen=Bot.mergeHash(screen,patch)
-		@users.update_profile(user.sig,'{"share_experiment":1}');
+		@users.update_profile(user.sig,'{"share_experiment_2":1}');
 		return self.get_screen(screen,user,msg)
 	end
 
 	def home_share_experiment_no_cb(msg,user,screen)
 		Bot.log.debug "#{__method__}"
-		@users.update_profile(user.sig,'{"share_experiment":0}');
+		@users.update_profile(user.sig,'{"share_experiment_2":0}');
 		return self.get_screen(screen,user,msg)
 	end
 
@@ -513,62 +424,26 @@ END
 			screen=self.find_by_name("home/vote_paused",self.get_locale(user))
 			return self.get_screen(screen,user,msg)
 		end
-		token_11={
+		token_2={
 			:iss=> CC_APP_ID_FB,
 			:sub=> Digest::SHA256.hexdigest(user.id.to_s+'@facebook.com'),
 			:email=> user.id.to_s+'@facebook.com',
 			:lastName=> user.last_name,
 			:firstName=> user.first_name,
-			:authorizedVotes=> [FB_VOTE_ID_11],
+			:authorizedVotes=> [FB_VOTE_ID_2],
 			:exp=>(Time.new.getutc+VOTING_TIME_ALLOWED).to_i
 		}
-		vote_token_11=JWT.encode token_11, CC_SECRET_FB, 'HS256'
-		vote_url_11=screen[:attachment]["payload"]["elements"][0]["buttons"][0]["url"].split('?')[0]
+		vote_token_2=JWT.encode token_2, CC_SECRET_FB, 'HS256'
+		vote_url_2=screen[:attachment]["payload"]["elements"][0]["buttons"][0]["url"].split('?')[0]
 		patch={
 			:attachment=>{
 				"payload"=>{
 					"elements"=>[
 						{
-							"buttons"=>[{"url"=>vote_url_11+"?token=#{vote_token_11}"}],
+							"buttons"=>[{"url"=>vote_url_2+"?token=#{vote_token_2}"}],
 							"default_action"=>{
-								"url"=>vote_url_11+"?token=#{vote_token_11}",
-								"fallback_url"=>vote_url_11+"?token=#{vote_token_11}"
-							}
-						}
-					]
-				}
-			}
-		}
-		custom_screen=Bot.mergeHash(screen,patch)
-		return self.get_screen(custom_screen,user,msg)
-	end
-
-	def home_non_official_candidates_cb(msg,user,screen)
-		Bot.log.debug "#{__method__}"
-		if VOTE_PAUSED then
-			screen=self.find_by_name("home/vote_paused",self.get_locale(user))
-			return self.get_screen(screen,user,msg)
-		end
-		token_4={
-			:iss=> CC_APP_ID_FB,
-			:sub=> Digest::SHA256.hexdigest(user.id.to_s+'@facebook.com'),
-			:email=> user.id.to_s+'@facebook.com',
-			:lastName=> user.last_name,
-			:firstName=> user.first_name,
-			:authorizedVotes=> [FB_VOTE_ID_4],
-			:exp=>(Time.new.getutc+VOTING_TIME_ALLOWED).to_i
-		}
-		vote_token_4=JWT.encode token_4, CC_SECRET_FB, 'HS256'
-		vote_url_4=screen[:attachment]["payload"]["elements"][0]["buttons"][0]["url"].split('?')[0]
-		patch={
-			:attachment=>{
-				"payload"=>{
-					"elements"=>[
-						{
-							"buttons"=>[{"url"=>vote_url_4+"?token=#{vote_token_4}"}],
-							"default_action"=>{
-								"url"=>vote_url_4+"?token=#{vote_token_4}",
-								"fallback_url"=>vote_url_4+"?token=#{vote_token_4}"
+								"url"=>vote_url_2+"?token=#{vote_token_2}",
+								"fallback_url"=>vote_url_2+"?token=#{vote_token_2}"
 							}
 						}
 					]
